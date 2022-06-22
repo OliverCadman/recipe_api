0e4b406787c9f92b55dc32536141ae3ef7ad7740
Oliver Cadman <o.cadman@live.co.uk>
2022-06-22 20:23:21 +0100

    Add user models, with unit tests
    Overridden Django default User model, to specify an email and password
    field only.
    
    Provides a simpler sign in, and also necessary to create a custom user model
    before making first migrations, in the case that we might want to change
    anything in future.Add user models, with unit tests
    
    Overridden Django default User model, to specify an email and password
    field only.
    
    Provides a simpler sign in, and also necessary to create a custom user model
    before making first migrations, in the case that we might want to change
    anything in future.

---
+++ app/core/models.py
@@ -0,0 +1,50 @@
+"""
+Database models.
+"""
+
+from django.db import models
+from django.contrib.auth.models import (
+        AbstractBaseUser,
+        BaseUserManager,
+        PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """ Manager for user models """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user model """
+
+        if not email:
+            raise ValueError(
+                "Please provide an email address."
+            )
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        """ Create and return a new superuser """
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """ Override default User model """
+
+    email = models.EmailField(max_length=244, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
