3132b7d5820c1e87b0d54dc23ef2b901463cecd2
Oliver Cadman <o.cadman@live.co.uk>
2022-08-03 11:31:24 +0100

    Create Ingredient model and view
    Updating and deleting ingredient functionality

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -51,6 +51,25 @@ class TagViewSet(mixins.UpdateModelMixin,
 
     def get_queryset(self):
         """Filter queryset to authenticated user."""
+        return self.queryset.filter(
+            user=self.request.user
+        ).order_by('-name')
+
+
+class IngredientViewSet(mixins.UpdateModelMixin,
+                        mixins.DestroyModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """View for managing ingredients API"""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user only."""
+
         return self.queryset.filter(
             user=self.request.user
         ).order_by('-name')
